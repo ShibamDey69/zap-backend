cd70ef5eb68199349a81c9fe1aa29c25b35b1d57
Shibam <123672103+ShibamDey69@users.noreply.github.com>
2026-06-24 12:30:44 +0000

    Database set up

---
+++ src/modules/auth/auth.repository.js
@@ -0,0 +1,43 @@
+export default class AuthRepository {
+  constructor(prisma) {
+    this.prisma = prisma;
+  }
+  async findById(id) {
+    return await this.prisma.auth.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async findByEmail(email) {
+    return await this.prisma.auth.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+
+  async create(data) {
+    return await this.prisma.auth.create({
+      data,
+    });
+  }
+
+  async update(id, data) {
+    return await this.prisma.auth.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  async delete(id) {
+    return await this.prisma.auth.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
---
+++ src/modules/auth/auth.service.js
@@ -0,0 +1,20 @@
+import prisma from "../../plugins/prisma.js";
+import AuthRepository from "./auth.repository.js";
+
+export default class AuthService {
+  constructor() {
+    this.repository = new AuthRepository(prisma);
+  }
+
+  async register(data) {
+    try {
+      const existingUser = await this.repository.findByEmail(data.email);
+      if (existingUser) {
+        throw new Error("User already exists");
+      }
+      return await this.repository.create(data);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
